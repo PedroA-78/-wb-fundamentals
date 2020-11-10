652bf167e386862a4955698e7e7f756bf27aa432
PedroA-78 <67925461+PedroA-78@users.noreply.github.com>
2020-11-10 05:10:08 -0300

    Aula 236 a 246 - Dom

---
+++ -class-files/Javascript/Framework/beluga.js
@@ -0,0 +1,8 @@
+function CalcM(arr) {
+    let result = 0
+    for (let i = 0; i <= arguments.length - 1; i++) {
+        result += arguments[i]
+    }
+    result /= arguments.length
+    return result
+}
