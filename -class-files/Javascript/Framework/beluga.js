2cd89b08b8f8851228c6ee7b86dc7b2b24403160
PedroA-78 <67925461+PedroA-78@users.noreply.github.com>
2020-11-13 05:20:51 -0300

    Aula 294 - Exercício

--- -class-files/Javascript/Framework/beluga.js
+++ -class-files/Javascript/Framework/beluga.js
@@ -1,10 +1,21 @@
 function CalcM(arr) {
+    if (Array.isArray(arr)) {
+        if (arr.length <= 1) {
+            return "Você não inseriu números o sulficiente para o cálculo"
+        } else {
+            let calc = arr.reduce(function(num, newNum){
+                return  num + newNum
+            })
+            calc /= arr.length
+            return calc.toFixed(1)
+        }
+    }
     let result = 0
     for (let i = 0; i <= arguments.length - 1; i++) {
         result += arguments[i]
     }
     result /= arguments.length
-    return result.toFixed(2)
+    return result.toFixed(1)
 }
 
 function raffle(max) {
