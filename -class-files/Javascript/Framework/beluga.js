673034c26cd3c6abf202ab2c96f9c48377f90844
PedroA-78 <67925461+PedroA-78@users.noreply.github.com>
2020-11-13 06:52:16 -0300

    Aula 297 a 299 - Array exercício

--- -class-files/Javascript/Framework/beluga.js
+++ -class-files/Javascript/Framework/beluga.js
@@ -1,21 +1,27 @@
 function CalcM(arr) {
-    if (Array.isArray(arr)) {
-        if (arr.length <= 1) {
-            return "Você não inseriu números o sulficiente para o cálculo"
-        } else {
-            let calc = arr.reduce(function(num, newNum){
-                return  num + newNum
-            })
-            calc /= arr.length
-            return calc.toFixed(1)
+    // if (Array.isArray(arr)) {
+    //     alert("array")
+    //     if (arr.length <= 1) {
+    //         return "Você não inseriu números o sulficiente para o cálculo"
+    //     } else {
+    //         let calc = arr.reduce(function(num, newNum){
+    //             return  num + newNum
+    //         })
+    //         calc /= arr.length
+    //         return calc.toFixed(1)
+    //     }
+    // }
+    if (arguments.length <= 1) {
+        return "Você não inseriu números o sulficiente para o cálculo"
+    } else {
+        let result = 0
+        for (let i = 0; i <= arguments.length - 1; i++) {
+            result += arguments[i]
         }
+        result /= arguments.length
+        return result.toFixed(1)
     }
-    let result = 0
-    for (let i = 0; i <= arguments.length - 1; i++) {
-        result += arguments[i]
-    }
-    result /= arguments.length
-    return result.toFixed(1)
+    
 }
 
 function raffle(max) {
